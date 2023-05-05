72897f07b4b6e486c9ebd82e701f5547de866952
abhay <abhaysp95@protonmail.com>
2023-05-05 13:33:54 +0530

    feat: structuring and basic validation for shortening URL

--- api/helpers/helpers.go
+++ api/helpers/helpers.go
@@ -1,1 +1,9 @@
 package helpers
+
+func ResolveDomainError(url string) bool {
+	return false
+}
+
+func EnforceHTTPS(url string) string {
+	return url
+}
--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,18 +1,20 @@
 package routes
 
 import (
+	"net/url"
 	"time"
 
+	"github.com/abhaysp95/chiisana_url/api/helpers"
 	"github.com/gofiber/fiber/v2"
 )
 
-type Request struct {
+type request struct {
 	URL     string        `json:"url"`
 	ShortAs string        `json:"short_as"`
 	Expiry  time.Duration `json:"expiry"`
 }
 
-type Response struct {
+type response struct {
 	URL            string        `json:"url"`
 	ShortAs        string        `json:"short_as"`
 	Expiry         time.Duration `json:"expiry"`
@@ -21,5 +23,27 @@ type Response struct {
 }
 
 func ShortenURL(ctx *fiber.Ctx) error {
+	body := &request{}
+
+	// parse the request body
+	if err := ctx.BodyParser(body); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse JSON"})
+	}
+
+	// enforce rate limiting
+
+	// check for request passed for URL shortening contains actual URL
+	if _, err := url.ParseRequestURI(body.URL); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"Invalid URL passed"})
+	}
+
+	// check domain error
+	if !helpers.ResolveDomainError(body.URL) {
+		return ctx.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error":"Domain error"})
+	}
+
+	// enforce HTTPs
+	body.URL = helpers.EnforceHTTPS(body.URL)
+
 	return nil
 }
